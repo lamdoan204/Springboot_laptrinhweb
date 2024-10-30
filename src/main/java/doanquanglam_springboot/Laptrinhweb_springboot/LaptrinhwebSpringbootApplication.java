57a953acf01e96ff317a45df381f69723850265f
lamdoan204 <22133032@student.hcmute.edu.vn>
2024-10-30 09:02:10 +0700

    tao repo

---
+++ src/main/java/doanquanglam_springboot/Laptrinhweb_springboot/LaptrinhwebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package doanquanglam_springboot.Laptrinhweb_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptrinhwebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptrinhwebSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/doanquanglam_springboot/Laptrinhweb_springboot/LaptrinhwebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package doanquanglam_springboot.Laptrinhweb_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptrinhwebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
